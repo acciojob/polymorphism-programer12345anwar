bd827f03c0d08f1ed843a8ac398ee502dd43cf6d
programer12345anwar <mdanwar40212@gmail.com>
2025-01-11 23:39:58 +0530

    Initial commit,It's an assignment work

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -2,4 +2,28 @@ package com.driver;
 
 public class Main {
 
+    class Product{
+
+        public void product(int x,int y){
+            System.out.println(x*y);
+        }
+
+        public void product(int x,int y,int z){
+            System.out.println(x*y*z);
+        }
+
+        public void product(double x,double y){
+            System.out.println(x*y);
+        }
+
+
+
+    }
+    public static void main(String[] args){
+        Product p = new Main().new Product();
+        p.product(2,3);
+        p.product(2,3,4);
+        p.product(2.0,3.0);
+    }
+
 }
